cccb2050b720aaf1af97cdb5ec613f9082fac983
guyfedwards <guyfedwards@gmail.com>
2017-08-22 15:11:48 +0100

    Initial commit

---
+++ main.rb
@@ -0,0 +1,25 @@
+require 'socket'
+require 'isin_code_generator'
+
+server = TCPServer.new('localhost', 2345)
+
+loop do
+    socket = server.accept
+
+    request = socket.gets
+
+    STDERR.puts request
+
+    response = IsinCodeGenerator.make("LU")
+
+    socket.print "HTTP/1.1 200 OK\r\n" +
+                 "Content-Type: text-plain\r\n" +
+                 "Content-Length: #{response.bytesize}\r\n" +
+                 "Connection: close\r\n"
+
+    socket.print "\r\n"
+
+    socket.print response
+
+    socket.close
+end
